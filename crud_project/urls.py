6fe59f8bddb51efdf690f3d6c7342b9643f03927
Chris32cm <cristofersosa159@gmail.com>
2025-07-24 22:48:06 -0500

    Prueba CI #1

--- crud_project/urls.py
+++ crud_project/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('productos.urls')),  # 👈 Incluye tus rutas
+    path('', include('productos.urls')),
 ]
